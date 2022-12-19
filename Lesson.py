cf8e2913374de289efa0a12f438305acffb73d6d
unknown <saule.tapenova@mail.ru>
2022-12-19 12:03:10 +0600

    lesson1

---
+++ Lesson.py
@@ -0,0 +1,62 @@
+#value = None
+#print(type(value))
+a = 123
+b = 1.23
+#print (type(a))
+#print (type(b))
+#value = 12334
+#print (value)
+s = 'hello world'
+#print (s)
+#s = "hello 'world"
+#print (s)
+# комментарий
+#s = 'hello "world'
+#print (s)
+#s = 'hello \nworld'
+#print (s) # вывод строки
+#print(a, ' - ',b, ' - ', s)
+#print ('{1} - {2} - {0}'.format(a, b, s))
+#print (f'{a} - {b} - {s}')
+
+#f = True
+#print(f)
+#list = [] # массив
+#print(list)
+#list = [1,2,3]
+#print(list)
+#list = ['1', '2', '3', 'hello', 1,2,4.5,True]  # массив строк
+#print(list)
+
+# Ввод и вывод данных
+# print()-вывод данных
+# input()-ввод данных(от пользователя)
+# print('Введите a');
+# a = input()
+# print('Введите b');
+# b = input()
+# print(a, b)
+# print(a, ' + ' , b, ' = ', a+b)
+# print('Введите a');
+# a = int(input()) # float
+# print('Введите b');
+# b = int(input())
+# print(a, ' + ' , b, ' = ', a+b) # сумма
+
+# Арифметические операции (+, -, / для вещест чисел, 
+# // в целых числах, % остаток от деления, 
+# ** возведение в квадрат, * умножение)
+# a =  +123
+# b =  -524
+# c = a+b
+# print(c)
+# a = 3
+# a = a + 5   a += 5
+# a = 1.3261231223
+# b = 3
+# c = round(a * b) # округление
+# c = round (a * b, 5) # до скольки знаков
+# print(c)
+# Логические операции
+a = 1 + 4
+print (a)
